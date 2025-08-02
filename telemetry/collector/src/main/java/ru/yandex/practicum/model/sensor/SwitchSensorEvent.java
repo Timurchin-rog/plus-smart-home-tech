37d8dc2469175145885fde1745b87844de7daeac
Timur Myusov <drakonhftfg@yandex.ru>
2025-08-02 15:41:30 +0700

    Локально тесты прошли. Освоен паттерн "Стратегия". Композиция рулит!

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/sensor/SwitchSensorEvent.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.model.sensor;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import ru.yandex.practicum.model.SensorEvent;
+import ru.yandex.practicum.model.sensor.enums.SensorEventType;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SwitchSensorEvent extends SensorEvent {
+    Boolean state;
+
+    @Override
+    public SensorEventType getType() {
+        return SensorEventType.SWITCH_SENSOR_EVENT;
+    }
+}
